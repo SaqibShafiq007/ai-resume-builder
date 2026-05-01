4e01b82cac4d06ef0c67fa9b3e8c44b4284a13fb
SaqibShafiq007 <saqibshafiq618@gmail.com>
2026-05-01 12:02:56 +0500

    create Hero Section and Banner

---
+++ client/src/components/home/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <div class="w-full py-2.5 font-medium text-sm text-white text-center bg-gradient-to-r from-violet-500 via-[#9938CA] to-[#E0724A]">
+        <p>New Ai feature Added</p>
+    </div>
+  )
+}
+
+export default Banner
