15efd7114dc361402bf0fc9b6ca891e8bc5727ca
SaqibShafiq007 <saqibshafiq618@gmail.com>
2026-05-01 10:21:14 +0500

    initialie repo on git and push basic pages

---
+++ client/src/App.jsx
@@ -0,0 +1,36 @@
+
+import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Layout from './pages/Layout'
+import Dashboard from './pages/Dashboard'
+import ResumeBuilder from './pages/ResumeBuilder'
+import Preview from './pages/Preview'
+import Login from './pages/Login'
+
+function App() {
+  return (
+    <>
+       <Routes>
+          <Route path='/' element = {<Home/>}/>
+
+          <Route path='app' element = {<Layout/>}>
+            <Route path='index' element = {<Dashboard/>}/>
+            <Route path='builder/:resumeId' element = {<ResumeBuilder/>}/>
+          </Route>
+
+          <Route path='view/:resumeId' element = {<Preview/>}/>
+          <Route path='login' element = {<Login/>}/>
+
+       </Routes>
+    
+    </>
+  )
+}
+
+export default App
+
+/*
+If something has children inside it → opening + closing tag
+**If something has no children → self closing /> **
+*/
---
+++ client/src/pages/ResumeBuilder.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ResumeBuilder() {
+  return (
+    <div>
+      <h1>resume builder</h1>
+    </div>
+  )
+}
+
+export default ResumeBuilder
